f6fdc74b379df86b741caa77fd81eccbb0e4b252
Joe Rybarczyk <joerybarczyk99@gmail.com>
2024-12-07 03:11:21 -0500

    refactor: 2024-6 recursion

--- src/utils/bridge/index.ts
+++ src/utils/bridge/index.ts
@@ -1,111 +1,17 @@
-interface TreeNode {
-    value: number;
-    add: TreeNode | null
-    multiply: TreeNode | null
-}
-
-interface ConcatTreeNode {
-    value: number;
-    add: TreeNode | null
-    multiply: TreeNode | null
-    concat: TreeNode | null;
-}
-
 export type Equation = {
     testValue: number,
     nums: number[],
 }
 
-function buildOperationTree(nums: number[]) {
-    return buildOperationTreeNode(nums.slice(1), nums[0]);
-}
-
-function buildOperationTreeNode(nums: number[], value = 0) {
+function getEquationSolutions(nums: number[], value = 0): number[] {
     if (nums.length === 0) {
-        return {
-            value,
-            add: null,
-            multiply: null,
-        }
-    }
-
-    const node: TreeNode = {
-        value,
-        add: buildOperationTreeNode(nums.slice(1), value + nums[0]),
-        multiply: buildOperationTreeNode(nums.slice(1), value * nums[0])
-    }
-
-    return node;
-}
-
-function buildConcatTree(nums: number[]) {
-    return buildConcatTreeNode(nums.slice(1), nums[0]);
-}
-
-function buildConcatTreeNode(nums: number[], value = 0) {
-    if (nums.length === 0) {
-        return {
-            value,
-            add: null,
-            multiply: null,
-            concat: null,
-        }
-    }
-
-    const node: ConcatTreeNode = {
-        value,
-        add: buildConcatTreeNode(nums.slice(1), value + nums[0]),
-        multiply: buildConcatTreeNode(nums.slice(1), value * nums[0]),
-        concat: buildConcatTreeNode(nums.slice(1), parseInt(`${value}${nums[0]}`))
-    }
-
-    return node;
-}
-
-function getLeafValuesAtDepth(node: TreeNode | null, currentDepth: number, maxDepth: number): number[] {
-    if (!node) {
-        return []
-    }
-    if (currentDepth === maxDepth) {
-        return [node.value]
+        return [value];
     }
 
     return [
-        ...getLeafValuesAtDepth(node.add, currentDepth + 1, maxDepth),
-        ...getLeafValuesAtDepth(node.multiply, currentDepth + 1, maxDepth)
+        ...getEquationSolutions(nums.slice(1), value + nums[0]),
+        ...getEquationSolutions(nums.slice(1), value * nums[0]),
     ]
-}
-
-function getLeafValues(node: TreeNode, depth: number) {
-    return getLeafValuesAtDepth(node, 0, depth);
-}
-
-function getConcatLeafValuesAtDepth(node: ConcatTreeNode | null, currentDepth: number, maxDepth: number): number[] {
-    if (!node) {
-        return []
-    }
-    if (currentDepth === maxDepth) {
-        return [node.value]
-    }
-
-    return [
-        // @ts-ignore
-        ...getConcatLeafValuesAtDepth(node.add, currentDepth + 1, maxDepth),
-        // @ts-ignore
-        ...getConcatLeafValuesAtDepth(node.multiply, currentDepth + 1, maxDepth),
-        // @ts-ignore
-        ...getConcatLeafValuesAtDepth(node.concat, currentDepth + 1, maxDepth),
-    ]
-}
-
-function getConcatLeafValues(node: ConcatTreeNode, depth: number) {
-    return getConcatLeafValuesAtDepth(node, 0, depth);
-}
-
-function getEquationSolutions(nums: number[]) {
-    const tree = buildOperationTree(nums);
-    const treeDepth = nums.length - 1;
-    return getLeafValues(tree, treeDepth);
 }
 
 export function isPossibleEquationSolution(equation: Equation) {
@@ -120,10 +26,16 @@ export function isPossibleEquationSolution(equation: Equation) {
     return false;
 }
 
-function getConcatEquationSolutions(nums: number[]) {
-    const tree = buildConcatTree(nums);
-    const treeDepth = nums.length - 1;
-    return getConcatLeafValues(tree, treeDepth);
+function getConcatEquationSolutions(nums: number[], value = 0): number[] {
+    if (nums.length === 0) {
+        return [value];
+    }
+
+    return [
+        ...getConcatEquationSolutions(nums.slice(1), value + nums[0]),
+        ...getConcatEquationSolutions(nums.slice(1), value * nums[0]),
+        ...getConcatEquationSolutions(nums.slice(1), parseInt(`${value}${nums[0]}`))
+    ]
 }
 
 export function isPossibleConcatSolution(equation: Equation) {
